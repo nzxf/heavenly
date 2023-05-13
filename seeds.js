8470938b61da2f1e4fccd6507a0a48d1a9056c86
nzxf <122195155+nzxf@users.noreply.github.com>
2023-05-13 19:02:08 +0700

    Initial commit

---
+++ seeds.js
@@ -0,0 +1,70 @@
+// 0AN (after nibiru) = year 2024
+
+const characters = [
+    {
+        id: "c1",
+        name: "maru",
+        ability: ["martial arts", "high durabiltiy", "hiru-sense", "maru-touch"],
+        image: "https://static.wikia.nocookie.net/heavenly-delusion/images/f/f8/Maru.png",
+        timeline: [
+            {
+                year: 2024,
+                description: "baby maru born",
+                image: ""
+            },
+            {
+                year: 2039,
+                description: "maru accompanied by kiruko travels to find heaven",
+                image: ""
+            }
+        ]
+    },
+    {
+        id: "c2",
+        name: "kiruko",
+        ability: ["marksman", "kiru-beam", "tactical fighter"],
+        image: "https://static.wikia.nocookie.net/heavenly-delusion/images/1/1b/Kiruko.png",
+        timeline: [
+            {
+                year: 2034,
+                description: "kiruko wake up after brain transplant",
+                image: ""
+            },
+            {
+                year: 2035,
+                description: "kiruko get out of hospital",
+                image: ""
+            },
+            {
+                year: 2039,
+                description: "kiruko meets maru and travels together to find heaven",
+                image: ""
+            },
+        ]
+    },
+    {
+        id: "c3",
+        name: "kuku",
+        ability: ["vertical surface climbing"],
+        image: "https://static.wikia.nocookie.net/heavenly-delusion/images/a/af/Kuku.png",
+        timeline: [
+            {
+                year: 2024,
+                description: "kuku",
+                image: ""
+            },
+            {
+                year: 2028,
+                description: "kuku got sick and then died",
+                image: ""
+            },
+            {
+                year: 2039,
+                description: "hiruko fish attack boat, then killed by maru",
+                image: ""
+            },
+        ]
+    }
+]
+
+export { characters }
