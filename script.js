73a5a48b04aa264c9e2840d2e07a33d2878c086f
nzxf <122195155+nzxf@users.noreply.github.com>
2023-05-15 12:10:19 +0700

    Redesign timeline container card

--- script.js
+++ script.js
@@ -1,27 +1,43 @@
 import { characters } from "./seeds.js";
 
-const callFive = number => {
-    let maxNumb = characters.length - 1
-    let result = []
-    switch (number) {
-        case 0:
-            result.push(maxNumb - 1, maxNumb, number, number + 1, number + 2)
-            break;
-        case 1:
-            result.push(maxNumb, number - 1, number, number + 1, number + 2)
-            break;
-        case characters.length - 2:
-            result.push(number - 2, number - 1, number, number + 1, 0)
-            break;
-        case maxNumb:
-            result.push(number - 2, number - 1, number, 0, 1)
-            break;
-        default:
-            result.push(number - 2, number - 1, number, number + 1, number + 2)
+let errPic = "https://developers.google.com/static/maps/documentation/maps-static/images/error-image-generic.png";
+
+// const callFive = number => {
+//     let maxNumb = characters.length - 1
+//     let result = []
+//     switch (number) {
+//         case 0:
+//             result.push(maxNumb - 1, maxNumb, number, number + 1, number + 2)
+//             break;
+//         case 1:
+//             result.push(maxNumb, number - 1, number, number + 1, number + 2)
+//             break;
+//         case characters.length - 2:
+//             result.push(number - 2, number - 1, number, number + 1, 0)
+//             break;
+//         case maxNumb:
+//             result.push(number - 2, number - 1, number, 0, 1)
+//             break;
+//         default:
+//             result.push(number - 2, number - 1, number, number + 1, number + 2)
+//     }
+//     return result
+// }
+
+
+const numAround = (arr, mid, around = 2) => {
+    let result = [];
+    for (let i = mid - around; i <= around; i++) {
+        if (i < 0) {
+            result.push(i + arr.length)
+        } else if (i > arr.length - 1) {
+            result.push(i - arr.length)
+        } else {
+            result.push(i)
+        }
     }
     return result
 }
-
 
 
 const addChar = id => {
@@ -39,21 +55,12 @@ const addChar = id => {
 
     // MID CONTAINER
     const midContainer = document.querySelector('.mid-container')
-    let ids = callFive(id)
+
     // NAMES
-    for (let i = 0; i < ids.length; i++) {
-        if (i === Math.floor(ids.length / 2)) {
-            const nameButton = document.createElement("BUTTON")
+    const nameButton = document.createElement("BUTTON")
             nameButton.classList.add("character-name", "character-name-focus")
-            nameButton.textContent = characters[ids[i]].name.toUpperCase()
+            nameButton.textContent = characters[id].name.toUpperCase()
             midContainer.append(nameButton)
-        } else {
-            const nameButton = document.createElement("BUTTON")
-            nameButton.classList.add("character-name")
-            nameButton.textContent = characters[ids[i]].name.toUpperCase()
-            midContainer.append(nameButton)
-        }
-    }
 
     // BOTTOM CONTAINER
     const bottomContainer = document.querySelector('.bottom-container')
@@ -68,19 +75,30 @@ const addChar = id => {
         timelineYear.classList.add("timeline-year")
         timelineYear.textContent = timeline[i].year
         timelineContainer.append(timelineYear)
-        
+
+        // YEAR
         let an = timeline[i].year - 2024
-        let n = (an < 2) ? "year" : "years"
+        let yearN = (an < 0) ? an * an : an
+        let yearS = (an < 2) ? "year" : "years"
+        let beforeAfter = (an <= 0) ? "before" : "after"
         const timelineYearAN = document.createElement("DIV")
         timelineYearAN.classList.add("timeline-year-an")
-        timelineYearAN.textContent = `${an} ${n} after Planet Nibiru hits Earth`
+        timelineYearAN.textContent = `${yearN} ${yearS} ${beforeAfter} the Great Cataclysm`
         timelineContainer.append(timelineYearAN)
 
+        if (timeline[i].year === 'n') {
+            timelineYear.textContent = "2024, Nov 11th" // "Nov 11th 2024, 2:14am JST"
+            timelineYear.style.color = 'rgba(255, 0, 0, 0.74)'
+            timelineYearAN.textContent =   `The day of the Great Cataclysm`
+        }
+        
+        // IMAGE (TIMELINE)
         const timelineImage = document.createElement("DIV")
         timelineImage.classList.add("timeline-image")
-        timelineImage.style.backgroundImage = `url(${timeline[i].image})`; 
+        timelineImage.style.backgroundImage = `url(${errPic})` //`url(${timeline[i].image})`; 
         timelineContainer.append(timelineImage)
-        
+
+        // DESCRIPTION
         const timelineDescription = document.createElement("DIV")
         timelineDescription.classList.add("timeline-description")
         timelineDescription.textContent = timeline[i].description
@@ -88,4 +106,4 @@ const addChar = id => {
     }
 }
 
-addChar(2);
+addChar(0);
